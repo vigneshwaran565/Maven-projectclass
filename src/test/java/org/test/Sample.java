26ca8807fee1b70b4d92f8d8dbdcc4eda3c9b050
vigneshwaran565 <vigneshwaranr798@gmail.com>
2022-03-14 17:11:14 -1200

    project structure

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	private void test() {
+
+		System.out.println("1");
+		System.out.println("4");
+		System.out.println("5");
+		
+
+	}
+	
+
+}
